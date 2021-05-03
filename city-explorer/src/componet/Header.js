a6bd3eb44f3207adc38c5e0b84c3d2506465b292
Mohammad jaradat <morjaradat66@gmail.com>
2021-05-03 16:52:55 +0300

    done

---
+++ city-explorer/src/componet/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export class header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 id='header'>City Exploder</h1>
+            </div>
+        )
+    }
+}
+
+export default header;
