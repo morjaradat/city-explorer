376d12dece598a89022e3e8f2522a18102c1a941
Mohammad jaradat <morjaradat66@gmail.com>
2021-05-06 23:27:57 +0300

    solve lab 8 +lab 9

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './componet/Header';
 import Main from './componet/Main';
 import Footer from './componet/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
 
--- src/componet/Map.js
+++ src/componet/Map.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Image from 'react-bootstrap/Image'
 
- class Map extends React.Component {
+class Map extends React.Component {
     render() {
-        // console.log('map')
+
         return (
             <div>
-                <Image id='image' src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.props.lat},${this.props.lon}&zoom=10`} alt='' roundedCircle />
-{/* style={{visibility: this.state.visibility}} */}
+                <Image id='image' src={`https://maps.locationiq.com/v3/staticmap?key=pk.d36871f015649f915282f374cff76628&q&center=${this.props.lat},${this.props.lon}&zoom=10`} alt='' thumbnail  />
+
             </div>
         )
     }
